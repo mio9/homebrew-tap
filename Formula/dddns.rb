130760341c5dd1f771fe20f96aff82863bb3b5d7
MiO9 <admin@mio9.sh>
2026-09-02 05:55:26 +0000

    dddns 0.2.0

--- Formula/dddns.rb
+++ Formula/dddns.rb
@@ -2,14 +2,14 @@ class Dddns < Formula
   desc "DDNS client for any DNS provider"
   homepage "https://github.com/mio9/dddns"
   license "MIT"
-  version "0.1.3"
+  version "0.2.0"
 
   if Hardware::CPU.arm?
     url "https://github.com/mio9/dddns/releases/download/v#{version}/dddns_v#{version}_darwin_arm64.tar.gz"
-    sha256 "09392ed2a0c81d0a6ed5c0e15c9cf9ad3a15f31ff13971cb2041b611470d76aa"
+    sha256 "5c570a883c712bbd18bd20bd409135848685f17e852d2fc56180244cb48665bb"
   elsif Hardware::CPU.intel?
     url "https://github.com/mio9/dddns/releases/download/v#{version}/dddns_v#{version}_darwin_amd64.tar.gz"
-    sha256 "8f51d5df08c4c177c4a1486b415ec67cbefa75326ce804e7ba3e76ad2992cd0f"
+    sha256 "6af3fa3346b2ba0cd0d901dbfb6881e4fd5f3806df6bea52eb5cf38fa3ae9fc4"
   end
 
   def install
